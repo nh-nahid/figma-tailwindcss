d245c7baf3b246cdf5e3e9e7e48aaee301cb9e93
Nahid Hossain <nahid4510@gmail.com>
2025-01-31 02:38:48 +0600

    final layout complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,16 @@ export default {
     extend: {},
     colors: {
       primary: "#FF6551",
+      new: "#E37A3F",
       secondary: "#9F9F9F",
       emphasis: "#26335D",
       background:"#184C99",
       white: "#FFFFFF",
+      price: "#2435A1",
+      text: "#737373",
+      month: "#AAD1F5",
+      input: "#F8F8F8",
+      copybg: "#FAFAFA",
 
     }
   },
